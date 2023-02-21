9b77dea2c69d13f67ca301409be6411eb745a2e2
Daniel Nguyen <daniel.nguye@gmail.com>
2023-02-21 16:48:07 -0500

    displayPopularShows

--- js/script.js
+++ js/script.js
@@ -7,13 +7,19 @@ const fetchAPIData = async (endpoint) => {
   const API_KEY = '26f969320ef95a0822db299f3d7c1b36';
   const API_URL = 'https://api.themoviedb.org/3/';
 
+  showSpinner();
+
   const response = await fetch(`${API_URL}${endpoint}?api_key=${API_KEY}&language=en-US`);
 
   const data = await response.json();
 
+  hideSpinner();
+
   return data;
 };
 
+
+// Display popular movies
 const displayPopularMovies = async () => {
   const { results } = await fetchAPIData('movie/popular');
 
@@ -39,6 +45,40 @@ const displayPopularMovies = async () => {
   });
 };
 
+// Display popular tv shows
+const displayPopularShows = async () => {
+  const { results } = await fetchAPIData('tv/popular');
+
+  results.forEach((show) => {
+    const div = document.createElement('div');
+    div.classList.add('card');
+    div.innerHTML = `
+      <a href="tv-details.html?id=${show.id}">
+        <img
+          src="https://image.tmdb.org/t/p/w500${show.poster_path}"
+          class="card-img-top"
+          alt="${show.name}"
+        />
+      </a>
+      <div class="card-body">
+        <h5 class="card-title">${show.name}</h5>
+        <p class="card-text">
+          <small class="text-muted">Air Date: ${show.first_air_date}</small>
+        </p>
+      </div>
+    `;
+    document.querySelector('#popular-shows').appendChild(div);
+  });
+};
+
+function showSpinner() {
+  document.querySelector('.spinner').classList.add('show');
+}
+
+function hideSpinner() {
+  document.querySelector('.spinner').classList.remove('show');
+}
+
 // Highlight active link
 function highlightActiveLink() {
   const links = document.querySelectorAll('.nav-link');
@@ -57,7 +97,7 @@ function init() {
       displayPopularMovies();
       break;
     case '/shows.html':
-      console.log('Shows');
+      displayPopularShows();
       break;
     case '/movie-details.html':
       console.log('Movie Details');
