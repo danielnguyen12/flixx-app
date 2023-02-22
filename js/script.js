010f4539d11f2812d6cc0160454b27768b49f7d5
Daniel Nguyen <daniel.nguye@gmail.com>
2023-02-22 18:29:52 -0500

    displayShowDetails

--- js/script.js
+++ js/script.js
@@ -51,6 +51,8 @@ const displayMovieDetails = async () => {
 
   const movie = await fetchAPIData(`movie/${movieId}`);
 
+  displayBackgroundImage('movie', movie.backdrop_path);
+
   const div = document.createElement('div');
   div.innerHTML = `
   <div class="details-top">
@@ -120,6 +122,80 @@ const displayPopularShows = async () => {
   });
 };
 
+// Display show details
+const displayShowDetails = async () => {
+  const showId = window.location.search.split('=')[1];
+
+  const show = await fetchAPIData(`tv/${showId}`);
+
+  displayBackgroundImage('tv', show.backdrop_path);
+
+  const div = document.createElement('div');
+  div.innerHTML = `
+  <div class="details-top">
+          <div>
+            <img
+              src="https://image.tmdb.org/t/p/w500${show.poster_path}"
+              class="card-img-top"
+              alt="${show.name}"
+            />
+          </div>
+          <div>
+            <h2>${show.name}</h2>
+            <p>
+              <i class="fas fa-star text-primary"></i>
+              ${show.vote_average.toFixed(1)} / 10
+            </p>
+            <p class="text-muted">Last Air Date: ${show.last_air_date}</p>
+            <p>
+            ${show.overview}
+            </p>
+            <h5>Genres</h5>
+            <ul class="list-group">
+              ${show.genres.map((genre) => `<li>${genre.name}</li>`).join('')}
+            </ul>
+            <a href="${show.homepage}" target="_blank" class="btn">Visit show Homepage</a>
+          </div>
+        </div>
+        <div class="details-bottom">
+          <h2>show Info</h2>
+          <ul>
+            <li><span class="text-secondary">No. of episodes:</span> ${show.number_of_episodes}</li>
+            <li><span class="text-secondary">Last episode to air:</span> ${show.last_episode_to_air.name}</li>
+            <li><span class="text-secondary">Episode runtime:</span> ${show.episode_run_time} minutes</li>
+            <li><span class="text-secondary">Status:</span> ${show.status}</li>
+          </ul>
+          <h4>Production Companies</h4>
+          ${show.production_companies.map((company) => `<span>${company.name}</span>`).join(', ')}
+        </div>
+  `;
+
+  document.querySelector('#show-details').appendChild(div);
+};
+
+// Display backdrop on details pages
+function displayBackgroundImage(type, backgroundPath) {
+  const overlayDiv = document.createElement('div');
+  overlayDiv.style.background = `url(https://image.tmdb.org/t/p/original/${backgroundPath})`;
+
+  overlayDiv.style.backgroundSize = 'cover';
+  overlayDiv.style.backgroundPosition = 'center';
+  overlayDiv.style.backgroundRepeat = 'no-repeat';
+  overlayDiv.style.height = '100vh';
+  overlayDiv.style.width = '100vw';
+  overlayDiv.style.position = 'absolute';
+  overlayDiv.style.top = '0';
+  overlayDiv.style.left = '0';
+  overlayDiv.style.zIndex = '-1';
+  overlayDiv.style.opacity = '0.1';
+
+  if (type === 'movie') {
+    document.querySelector('#movie-details').appendChild(overlayDiv);
+  } else {
+    document.querySelector('#show-details').appendChild(overlayDiv);
+  }
+}
+
 function showSpinner() {
   document.querySelector('.spinner').classList.add('show');
 }
@@ -152,7 +228,7 @@ function init() {
       displayMovieDetails();
       break;
     case '/tv-details.html':
-      console.log('TV Details');
+      displayShowDetails();
       break;
     case '/search.html':
       console.log('Search');
